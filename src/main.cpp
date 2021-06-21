6bb55c49365ed0fa96f6c95eae50dfff4ad96abd
Brooke Chalmers <breq@breq.dev>
2021-06-21 18:09:05 -0400

    Add hardware I2C

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "main.h"
+#include "i2c.h"
+
 
 int main() {
     stdio_init_all();
+
+    i2c_setup();
+
     while (true) {
         printf(MESSAGE);
         sleep_ms(1000);
